0d549b7d5c84188f6da5213795e2c1aef3447cfd
Muntasir Ifaz <muntasir574@gmail.com>
2024-06-29 22:26:17 +0600

    Initial Commit

---
+++ WatherApp/WatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatherAppApp.swift
+//  WatherApp
+//
+//  Created by Muntasir Efaz on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct WatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
